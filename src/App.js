1a2c6f2e5bef97623b3a125ab60f01bec008f377
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-04 14:28:13 -0500

    adds error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,6 +7,10 @@ import './App.css';
 import axios from 'axios';
 const API_ENDPOINT= 'https://swapi.co/api/people/1/';
 
+
+function urlForId(id) {
+  return `https://swapi.co/api/people/${id}/`;
+}
 // axios.get(API_ENDPOINT)
 //     .then(response => {
 //         console.log(response.data);
@@ -24,21 +28,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'not their real name'
+      name: '',
+      currentId: 1
     }
   }
 
   componentDidMount() {
-    //thi sis the method that react calls after the ocmponent has been attacked ot the DOM as a real element 
+    //this is the method that react calls after the ocmponent has been attacked ot the DOM as a real element 
     // this is the first react method that is safe to call this.setState
-        axios.get(API_ENDPOINT)
-      .then(response => {
-          console.log(response.data.name);
-          // name = response.data.name;
-          this.setState({
-            name: response.data.name
-          })
-      })
+      //   axios.get(urlForId(this.state.currentId))
+      // .then(response => {
+      //     console.log(response.data.name);
+      //     // name = response.data.name;
+      //     this.setState({
+      //       name: response.data.name
+      //     })
+      // })
+      this._makeAjaxRequest();
   }
 
   render() {
@@ -51,10 +57,36 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.name}
+          {
+          this.state.name ? // || <img src={logo} /> //
+            this.state.name
+            :
+            <img src={logo} />
+          }
+          <button onClick={this._getNextCharacter}>Next</button>
         </header>
       </div>
     );
+  }
+
+  _getNextCharacter = () => {
+    this.setState({
+      currentId: this.state.currentId + 1
+    }, () => {
+      console.log(`New currentId is ${this.state.currentId}`);
+      this._makeAjaxRequest();
+    });
+  }
+
+  _makeAjaxRequest = () => {
+    axios.get(urlForId(this.state.currentId))
+    .then(response => {
+        console.log(response.data.name);
+        // name = response.data.name;
+        this.setState({
+          name: response.data.name
+        })
+    })
   }
 }
 
