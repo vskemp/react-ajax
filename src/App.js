7e250cd5370f1d59a131473bb7311994721f7793
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-04 13:52:41 -0500

    render json data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import axios from 'axios';
+const API_ENDPOINT= 'https://swapi.co/api/people/1/';
+
+// axios.get(API_ENDPOINT)
+//     .then(response => {
+//         console.log(response.data);
+//     })
+
+
+// without axios, just fetch
+// fetch(API_ENDPOINT)
+//     .then(r => r.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'not their real name'
+    }
+  }
+
+  componentDidMount() {
+    //thi sis the method that react calls after the ocmponent has been attacked ot the DOM as a real element 
+    // this is the first react method that is safe to call this.setState
+        axios.get(API_ENDPOINT)
+      .then(response => {
+          console.log(response.data.name);
+          // name = response.data.name;
+          this.setState({
+            name: response.data.name
+          })
+      })
+  }
+
+  render() {
+    // axios.get(API_ENDPOINT)
+    //   .then(response => {
+    //       console.log(response.data.name);
+    //       name = response.data.name;
+    //   })
+      
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.name}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* Asyncronous- JavaScript- And- XML "fetch"*/
+
+// import axios from 'axios';
+// const API_ENDPOINT= 'https://swapi.co/api/people/1/';
+
+// axios.get(API_ENDPOINT)
+//     .then(response => {
+//         console.log(response);
+//     })
+
+
+// without axios, just fetch
+// fetch(API_ENDPOINT)
+//     .then(r => r.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
